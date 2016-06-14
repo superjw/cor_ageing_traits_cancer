89baf1c01e4f902a99c2e92659a2299f9862df53
superxiaopobao <jw.wang@live.com>
2016-06-14 17:49:39 +0100

    first commit

--- 1k_flank_no_of_mapped_traits.py
+++ 1k_flank_no_of_mapped_traits.py
@@ -5,27 +5,36 @@ this script is used for count no_of_mapped_traits of each gene based on mapping 
 
 def get_set_of_ensg(association_file_with_mapping_obj):
     l = []
-    # expr = r''
     next(association_file_with_mapping_obj)
     for line in association_file_with_mapping_obj:
-        ensg = line.strip().split('\t')[36].replace(' ', '')
-        if not ensg:
+        # print(line)
+        try:
+            ensg = line.strip().split('\t')[36].replace(' ', '')
             if ',' in ensg:
                 ensg = ensg.split(',')
                 for i in ensg:
                     l.append(i)
             else:
                 l.append(ensg)
-        print('gene list set built!')
+            # print(ensg)
+            # print('===========')
+        except IndexError:
+            pass
+    print('gene list set built!')
+    # print(list(set(l)))
     return list(set(l))
 
 
-def get_number_of_asso_and_efo(ensembl_gene_id, asso_file_obj):
+def get_number_of_asso_and_efo(ensembl_gene_id):
+    inf = open('gwas_asso_file_1kb_flank_my_mapping.tsv', 'r')
     no_of_association = 0
     efo_list = []
-    next(asso_file_obj)
-    for line in asso_file_obj:
-        if ensembl_gene_id in line.strip().split('\t')[36].replace(' ', ''):
+    next(inf)
+    for line in inf:
+        print(line.strip())
+        # if ensembl_gene_id in line.strip().split('\t')[36].replace(' ', ''):
+        print(ensembl_gene_id)
+        if ensembl_gene_id in line:
             efo_link = line.strip().split('\t')[35].replace(' ', '')
             if ',' in efo_link:
                 for efo in efo_link:
@@ -34,24 +43,29 @@ def get_number_of_asso_and_efo(ensembl_gene_id, asso_file_obj):
                     else:
                         pass
             else:
-                if efo not in efo_list:
-                    efo_list.append(efo)
+                if efo_link not in efo_list:
+                    efo_list.append(efo_link)
                 else:
                     pass
             no_of_association += 1
+            print(no_of_association)
         else:
-            print(line)
+            # print(line)
             pass
+    inf.close()
     return str(no_of_association), str(len(efo_list))
 
 
 outfile = open('asso_efo_count_each_gene.tsv', 'w')
 outfile.write('ENSG\tasso_count\tefo_count/mapped_traits\n')
-with open('gwas_asso_file_1kb_flank_my_mapping.tsv', 'r') as f:
-    ensg_ids = get_set_of_ensg(f)
-    for ensg in ensg_ids:
-        asso_count, efo_count = get_number_of_asso_and_efo(ensg, f)
-        outfile.write(ensg + '\t' + asso_count + '\t' + efo_count + '\n')
-        # print(ensg + '\t' + asso_count + '\t' + efo_count)
+infile = open('gwas_asso_file_1kb_flank_my_mapping.tsv', 'r')
+ensg_ids = get_set_of_ensg(infile)
+# print(ensg_ids)
+for ensg in ensg_ids:
+    # print(ensg)
+    asso_count, efo_count = get_number_of_asso_and_efo(ensg)
+    outfile.write(ensg + '\t' + asso_count + '\t' + efo_count + '\n')
+    # print(ensg + '\t' + asso_count + '\t' + efo_count)
 outfile.close()
+infile.close()
 print('job done!')
