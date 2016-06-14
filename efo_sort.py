efcc1b956abdf5cbb99adc84ef3e1e7e7ccb9909
superxiaopobao <jw.wang@live.com>
2016-06-14 12:03:13 +0100

    fix the error of build dictionary once in each line

--- efo_sort.py
+++ efo_sort.py
@@ -48,12 +48,12 @@ def get_gid_gname_from_dict(rs_id_list, id_dict, name_dict):
 gene_id_dict, gene_name_dict = build_rs_gene_mapping_dict()
 outfile = open('gwas_asso_file_1kb_flank_my_mapping.tsv', 'w')
 with open('./03/gwas_catalog_associations_ontology-annotated.tsv', 'r') as f:
-    header = next(f)
+    header = next(f).strip()
     outfile.write(header + '\t1kb_mapped_gene_id\t1kb_mapped_gene_name\n')
     for line in f:
         rs = line.strip().split('\t')[21].split(',')
         if rs:
             gene_id_string, gene_name_sting = get_gid_gname_from_dict(rs, gene_id_dict, gene_name_dict)
-            outfile.write(line + '\t' + gene_id_string + '\t' + gene_name_sting + '\n')
+            outfile.write(line.strip() + '\t' + gene_id_string + '\t' + gene_name_sting + '\n')
 outfile.close()
 print('job done!')
